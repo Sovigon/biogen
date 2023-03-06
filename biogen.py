8232602fae79e798c31fa2cacd5257be07fdfe45
Sovigon <cctaylor29@gmail.com>
2023-03-05 23:12:16 -0500

    Made class structure for seeds

--- biogen.py
+++ biogen.py
@@ -1,5 +1,6 @@
 import pygame as pg
 import random as rand
+import string
 import cProfile as cP
 
 SCREEN_HEIGHT = 1000
@@ -25,11 +26,12 @@ clock = pg.time.Clock()
 
 
 class Seed(object):
-    def __init__(self, color_matrix, adjacent_range, tick_framerate, initiative):
+    def __init__(self, color_matrix, adjacent_range, tick_framerate, initiative, seed_id):
         self.color_matrix = color_matrix
         self.adjacent_range = adjacent_range
         self.tick_framerate = tick_framerate
         self.initiative = initiative
+        self.seed_id = seed_id
 
         self.seed_tick = tick_framerate
 
@@ -37,20 +39,86 @@ class Seed(object):
         self.seed_grid = []
         self.active_cells = []
 
+    def _name(self):
+        return self.__class__.__name__
 
-green_seed = Seed({"heart": [(5, 160, 30), 1], "grow": [
-                  (10, 200, 40), 2], "old": [(0, 100, 20), 3]}, 1, 4, 1)
 
-blue_seed = Seed({"heart": [(5, 30, 160), 4], "grow": [
-                 (10, 40, 200), 5], "old": [(0, 20, 100), 6]}, 3, 9, 2)
+class GreenSeed(Seed):
+    def __init__(self, unique_id="0"):
+        super().__init__({"heart": [(5, 160, 30), 1], "grow": [
+            (10, 200, 40), 2], "old": [(0, 100, 20), 3]}, 1, 3, 1, "Grn")
 
-red_seed = Seed({"heart": [(160, 30, 5), 7], "grow": [
-                 (200, 40, 10), 8], "old": [(100, 20, 0), 9]}, 2, 7, 3)
+        self.unique_id = unique_id
+        self.unique_name = self.seed_id + str(self.initiative) + unique_id
 
-yellow_seed = Seed({"heart": [(160, 160, 40), 10], "grow": [
-    (200, 200, 80), 11], "old": [(100, 100, 10), 12]}, 1, 2, 4)
 
-active_colors = []
+class BlueSeed(Seed):
+    def __init__(self, unique_id="0"):
+        super().__init__({"heart": [(5, 30, 160), 4], "grow": [
+                         (10, 40, 200), 5], "old": [(0, 20, 100), 6]}, 3, 9, 2, "Blu")
+
+        self.unique_id = unique_id
+        self.unique_name = self.seed_id + str(self.initiative) + unique_id
+
+
+class RedSeed(Seed):
+    def __init__(self, unique_id="0"):
+        super().__init__({"heart": [(160, 30, 5), 7], "grow": [
+                         (200, 40, 10), 8], "old": [(100, 20, 0), 9]}, 2, 7, 3, "Red")
+
+        self.unique_id = unique_id
+        self.unique_name = self.seed_id + str(self.initiative) + unique_id
+
+
+class YellowSeed(Seed):
+    def __init__(self, unique_id="0"):
+        super().__init__({"heart": [(160, 160, 40), 10], "grow": [
+            (200, 200, 80), 11], "old": [(100, 100, 10), 12]}, 1, 1, 4, "Ylw")
+
+        self.unique_id = unique_id
+        self.unique_name = self.seed_id + str(self.initiative) + unique_id
+
+
+seed_types = [cls()._name() for cls in Seed.__subclasses__()]
+# print(seed_types)
+
+seed_number = 7
+
+unique_seeds = []
+random_taken = []
+
+for unique in range(seed_number):
+    pick = rand.choice(seed_types)
+    while True:
+        random_id = ''.join(rand.choice(string.digits)
+                            for length in range(2))
+        if random_id not in random_taken:
+            break
+    random_taken.append(random_id)
+    unique_seeds.append((pick, random_id))
+
+# print(unique_seeds)
+
+active_seeds = []
+
+for activate, id in unique_seeds:
+    activate_seed = locals()[activate](id)
+    seed_name = activate_seed.seed_id + id
+    active_seeds.append((seed_name, activate_seed))
+
+unique_hearts = []
+
+for heart in active_seeds:
+    while True:
+        rand_x = rand.randint(0, MAX_ROW_LENGTH - 1)
+        rand_y = rand.randint(0, MAX_COLUMN_LENGTH - 1)
+        if [rand_x, rand_y] not in unique_hearts:
+            break
+    grid[rand_x][rand_y] = (heart[1].unique_name, heart[1].color_matrix["heart"][1])
+    heart[1].seed_heart.extend([rand_x, rand_y])
+    # print(f"{grid[rand_x][rand_y]} at {heart[1].seed_heart}")
+    heart[1].active_cells.append([rand_x, rand_y])
+    unique_hearts.append([rand_x, rand_y])
 
 
 def main():
@@ -61,25 +129,9 @@ def main():
 
     SCREEN.fill(BLACK)
 
-    active_seeds = [green_seed, blue_seed, red_seed, yellow_seed]
-
-    for seed in active_seeds:
-        rand_x = rand.randint(0, MAX_ROW_LENGTH - 1)
-        rand_y = rand.randint(0, MAX_COLUMN_LENGTH - 1)
-        grid[rand_x][rand_y] = seed.color_matrix["heart"][1]
-        seed.seed_heart.extend([rand_x, rand_y])
-
-        active_colors.append(seed.color_matrix)
-
-        for prelim in neighbors(seed.seed_heart, adjacent_range=seed.adjacent_range):
-            seed.active_cells.append(prelim)
-
-        for indi_cells in seed.active_cells:
-            grid[indi_cells[0]][indi_cells[1]] = seed.color_matrix["grow"][1]
+    ignit = True
 
     update_grid()
-
-    ignit = True
 
     while ignit:
         clock.tick(120)
@@ -89,57 +141,57 @@ def main():
             if event.type == pg.QUIT:
                 ignit = False
 
+        new_cells = []
+
         for seed_grow in active_seeds:
-            seed_grow.seed_tick -= 1
-            if seed_grow.seed_tick == 0:
+            current_seed = seed_grow[1]
+            current_seed.seed_tick -= 1
+            if current_seed.seed_tick == 0:
 
-                new_cells = []
+                for old_cells in current_seed.active_cells:
+                    current_seed.seed_grid.append(old_cells)
+                    grid[old_cells[0]][old_cells[1]] = (
+                        seed_grow[0], current_seed.color_matrix["old"][1])
 
-                for old_cells in seed_grow.active_cells:
-                    seed_grow.seed_grid.append(old_cells)
-                    grid[old_cells[0]][old_cells[1]] = seed_grow.color_matrix["old"][1]
+                    for grow_cells in neighbors(old_cells, new_cells, adjacent_range=current_seed.adjacent_range):
+                        add_cell = (seed_grow[0], grow_cells, seed_grow[1].color_matrix["grow"][1])
+                        for replace_cell in new_cells:
+                            if add_cell[1] == replace_cell[1]:
+                                new_cells.remove(replace_cell)
+                        new_cells.append(add_cell)
 
-                    for grow_cells in neighbors(old_cells, new_cells, adjacent_range=seed_grow.adjacent_range):
-                        new_cells.append(grow_cells)
+                current_seed.seed_tick = current_seed.tick_framerate
 
-                seed_grow.active_cells = new_cells
+        for assign_cells in new_cells:
+            for seed_assign in active_seeds:
+                if assign_cells[0] == seed_assign[0]:
+                    seed_assign[1].active_cells.append(assign_cells[1])
 
-                # print(f'\nCurrent growing cells for {seed_grow}:\n{seed_grow.active_cells}')
-                # print(f'Current grow color: {seed_grow.color_matrix["grow"][1]}')
+        # print(new_cells)
 
-                for indi_cells in seed_grow.active_cells:
-                    grid[indi_cells[0]][indi_cells[1]] = seed_grow.color_matrix["grow"][1]
-                    # print(f'Loop grow color: {seed_grow.color_matrix["grow"][1]}')
-                    # print(
-                    # f"Cell value of ({indi_cells[0]}, {indi_cells[1]}): {grid[indi_cells[0]][indi_cells[1]]}")
-
-                seed_grow.seed_tick = seed_grow.tick_framerate
-
-        update_grid()
+        update_grid(new_cells)
 
     pg.quit()
 
 
-def update_grid(seed_colors=active_colors, base_grid=grid):
+def update_grid(list_grid=grid):
 
-    WHITE = (160, 160, 160)
-
-    def grid_create(color=WHITE):
+    def grid_create(color=(160, 160, 160)):
 
         pg.draw.rect(SCREEN, color, [(BOX_SIZE + MARGIN) * column + MARGIN,
                                      (BOX_SIZE + MARGIN) * row + MARGIN, BOX_SIZE, BOX_SIZE])
 
-    for row in range(MAX_ROW_LENGTH):
-        for column in range(MAX_COLUMN_LENGTH):
-            if grid[row][column] == 0:
+    if list_grid == grid:
+        for row in range(MAX_ROW_LENGTH):
+            for column in range(MAX_COLUMN_LENGTH):
                 grid_create()
-            else:
-                for colors in seed_colors:
-                    for values in colors.values():
-                        if grid[row][column] == values[1]:
-                            # print(
-                                # f'Non 0 detected. Assigning cell with value {values[1]} with color {values[0]}')
-                            grid_create(values[0])
+    else:
+        for grid_cell in list_grid:
+            row = grid_cell[1][0]
+            column = grid_cell[1][1]
+            for color_check in active_seeds:
+                if grid_cell[0] == color_check[0]:
+                    grid_create(color_check[1].color_matrix["grow"][0])
 
 
 def neighbors(cell, new=[], adjacent_range=1, min_grid_y=0, min_grid_x=0, max_grid_x=MAX_COLUMN_LENGTH, max_grid_y=MAX_ROW_LENGTH):
